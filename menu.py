1a567816d9194bb4412c4d310bd43c6f789aacd3
Luis Alfredo Villamizar Lopez <136473748+gorillaz68@users.noreply.github.com>
2023-06-14 12:12:16 -0400

    Create menu.py
    Moulo funcional Limite tipo interfaz menu

---
+++ menu.py
@@ -0,0 +1,10 @@
+def menu():
+    print "1.-sumar";
+    print "2.-restar";
+    print "3.-multiplicar";
+    print "4.-dividir";
+    print "5.-salir:";
+    op=input();
+    print "selecione la opcion:",op;
+    return op
+    
